504f0988c2fef28d9864ce11bff5fe6026539a70
samar-guicha <samarguicha61@gmail.com>
2023-05-12 23:18:48 +0200

    first commit

--- GestionChansons/src/main/java/dao/Test.java
+++ GestionChansons/src/main/java/dao/Test.java
@@ -5,7 +5,6 @@ import dao.entities.Chanson;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		IGestionChanson gestion = new GestionChansonImpJpa();
 
 	}
