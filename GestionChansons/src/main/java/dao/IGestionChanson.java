816c1e6e121cbe4f60d6608f04abab3711acb6e8
samar-guicha <samarguicha61@gmail.com>
2023-05-12 23:09:55 +0200

    commit 1

--- GestionChansons/src/main/java/dao/IGestionChanson.java
+++ GestionChansons/src/main/java/dao/IGestionChanson.java
@@ -5,7 +5,7 @@ import java.util.List;
 import dao.entities.Chanson;
 
 public interface IGestionChanson {
-
+	
 	public void ajouterChanson(Chanson c);
 	public void supprimerChanson(int id);
 	public void mettreAJourChanson(Chanson c);
